b5aba4b2b2acedd135d0a26e411b1bb35823d6cc
Théo LEPINE <theo.lepine@gmail.com>
2018-09-24 21:02:30 +0200

    Avancée envoie msg client>server, nettoyage du code

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -5,71 +5,45 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
-struct sockaddr_in get_sockaddr(char** argv)
-{
-  struct sockaddr_in host_addr;
-  memset(&host_addr, '\0', sizeof(host_addr));
-
-  host_addr.sin_family = AF_INET;
-  host_addr.sin_port = htons(atoi(argv[2]));
-  inet_aton(argv[1], &host_addr.sin_addr);
-
-  return host_addr;
-}
-
-int do_socket()
-{
-  int sock = socket(AF_INET, SOCK_STREAM, 0); // TCP
-  if (sock == -1){
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
-  return sock;
-}
-
-int do_connect(int sock, struct sockaddr_in host_addr)
-{
-  int connect_result = connect(sock, (struct sockaddr *) &host_addr, sizeof(host_addr));
-  if(connect_result == -1)
-  {
-    perror("connect");
-    exit(EXIT_FAILURE);
-  }
-  printf("Connexion au serveur réussi\n\n");
-}
-
-ssize_t readline(int file_des, void *str, size_t maxlen)
-{
-  return read(file_des, str, maxlen);
-}
-
-ssize_t sendline(int file_des, const void *str, size_t maxlen)
-{
-  return write(file_des, str, maxlen);
-}
+void error(const char *msg);
+int do_socket();
+void init_client_addr(struct sockaddr_in *serv_addr, char *ip, int port);
+void do_connect(int sock, struct sockaddr_in host_addr);
+ssize_t readline(int file_des, void *str, size_t maxlen);
+ssize_t sendline(int file_des, const void *str, size_t maxlen);
 
 int main(int argc,char** argv)
 {
-  struct sockaddr_in host_addr;
-  int sock;
-
     if (argc != 3)
     {
         fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
         return 1;
     }
 
+    int host_port = atoi(argv[2]);
+    char *host_ip = argv[1];
+    if(host_port <= 1024)
+    {
+      fprintf(stderr, "Please use a non reserved port number.");
+      exit(EXIT_FAILURE);
+    }
+
+    int sock;
+    struct sockaddr_in host_addr;
+
 //get address info from the server
-  host_addr = get_sockaddr(argv);
+    init_client_addr(&host_addr, host_ip, host_port);
 
 
 //get the socket
-  sock = do_socket();
+    sock = do_socket();
 
 //connect to remote socket
-  do_connect(sock, host_addr);
+    do_connect(sock, host_addr);
 
 
 //get user input
@@ -92,4 +66,55 @@ send(sock, message, maxlen, 0);
     return 0;
 
 
+}
+
+void error(const char *msg)
+{
+  /* Interrupt program because of an error */
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+int do_socket()
+{
+  /* Create a socket and return the associated file descriptor */
+  int file_des = socket(AF_INET, SOCK_STREAM, 0);
+  if (file_des == -1) {
+    error("Error during socket creation");
+  }
+
+  return file_des;
+}
+
+void init_client_addr(struct sockaddr_in *serv_addr, char *ip, int port)
+ {
+   /* Modify specified sockaddr_in for the client side with specified port and IP */
+   // clean structure
+   memset(serv_addr, '\0', sizeof(*serv_addr));
+   serv_addr->sin_family = AF_INET; // IP V4
+   serv_addr->sin_port = htons(port); // specified port in args
+   serv_addr->sin_addr.s_addr = inet_addr(ip); // specified server IP in args
+ }
+
+ void do_connect(int sock, struct sockaddr_in host_addr)
+ {
+   int connect_result = connect(sock, (struct sockaddr *) &host_addr, sizeof(host_addr));
+   if (connect_result == -1) {
+     error("Error during connection");
+   }
+ }
+
+ssize_t readline(int file_des, void *str, size_t maxlen)
+{
+  /* Read a line from the file descriptor with a maximum length in the specified buffer */
+  read(file_des, str, maxlen);
+}
+
+ssize_t sendline(int file_des, const void *str, size_t maxlen)
+{
+  /* Write a line in the file descriptor with a maximum length with the given buffer */
+  int sent=0;
+  do {
+    sent += write(file_des, str + sent, maxlen - sent);
+  } while (sent != maxlen);
 }
--- src/jalon01/server.c
+++ src/jalon01/server.c
@@ -6,84 +6,56 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(EXIT_FAILURE);
-}
-
-int do_socket()
-{
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock == -1)
-    error("socket");
-  return sock;
-}
-
-struct sockaddr_in init_serv_addr(char** argv)
- {
-   struct sockaddr_in serv_addr;
-   // clean structure
-   memset(&serv_addr, '\0', sizeof(serv_addr));
-
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_port = htons(atoi(argv[1]));
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-   return serv_addr;
- }
-
-ssize_t readline(int file_des, void *str, size_t maxlen)
-{
-  return read(file_des, str, maxlen);
-}
-
-ssize_t sendline(int file_des, const void *str, size_t maxlen)
-{
-  return write(file_des, str, maxlen);
-}
+void error(const char *msg);
+int do_socket();
+void init_serv_addr(struct sockaddr_in *serv_addr, int port);
+void do_bind(int socket, struct sockaddr_in addr_in);
+void do_listen(int socket, int nb_max);
+ int do_accept(int socket, struct sockaddr *addr, socklen_t* addrlen);
+ssize_t readline(int file_des, void *str, size_t maxlen);
+ssize_t sendline(int file_des, const void *str, size_t maxlen);
 
 int main(int argc, char** argv)
 {
-
-    if (argc != 2)
-    {
+    if (argc != 2) {
         fprintf(stderr, "usage: RE216_SERVER port\n");
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
-    int sock, bind_result, listen_result;
+    int port = atoi(argv[1]);
+    if(port <= 1024) {
+      fprintf(stderr, "Please use a non reserved port number.");
+      exit(EXIT_FAILURE);
+    }
+
+    int sock;
     struct sockaddr_in serv_addr;
 
     //create the socket, check for validity!
     sock = do_socket();
 
-
     //init the serv_add structure
-    serv_addr = init_serv_addr(argv);
+    init_serv_addr(&serv_addr, port);
 
     //perform the binding
     //we bind on the tcp port specified
-    bind_result = bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    if (bind_result == -1) {
-      error("bind");
-    }
-
+    do_bind(sock, serv_addr);
 
     //specify the socket to be a server socket and listen for at most 20 concurrent client
-    listen_result = listen(sock, 20);
-    if (listen_result == -1) {
-      error("listen");
-    }
+    do_listen(sock, 20);
 
     for (;;)
     {
-
+        socklen_t addrlen = sizeof(serv_addr);
         //accept connection from client
-        //do_accept()
+        int connection_fd = do_accept(sock, (struct sockaddr*)&serv_addr, &addrlen);
 
         //read what the client has to say
         //do_read()
+        char message[30];
+        memset(message, '\0', 30);
+        readline(connection_fd, &message, 30);
+        printf("%s\n", message);
 
         //we write back to the client
         //do_write()
@@ -96,3 +68,74 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+void error(const char *msg)
+{
+  /* Interrupt program because of an error */
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+int do_socket()
+{
+  /* Create a socket and return the associated file descriptor */
+  int file_des = socket(AF_INET, SOCK_STREAM, 0);
+  if (file_des == -1) {
+    error("Error during socket creation");
+  }
+
+  return file_des;
+}
+
+void init_serv_addr(struct sockaddr_in *serv_addr, int port)
+ {
+   /* Modify specified sockaddr_in for the server side with specified port */
+   // clean structure
+   memset(serv_addr, '\0', sizeof(*serv_addr));
+   serv_addr->sin_family = AF_INET; // IP V4
+   serv_addr->sin_port = htons(port); // specified port in args
+   serv_addr->sin_addr.s_addr = INADDR_ANY;
+ }
+
+ void do_bind(int socket, struct sockaddr_in addr_in)
+ {
+   /* Perform a bind on specified socket */
+   int bind_result = bind(socket, (struct sockaddr *) &addr_in, sizeof(addr_in));
+   if (bind_result == -1) {
+     error("Error during socket binding");
+   }
+ }
+
+ void do_listen(int socket, int nb_max)
+ {
+   /* Switch specified socket in the listen state */
+   int listen_result = listen(socket, nb_max);
+   if (listen_result == -1) {
+     error("Error during socket listening");
+   }
+ }
+
+ int do_accept(int socket, struct sockaddr *addr, socklen_t* addrlen)
+ {
+   /* Accept a connection with the specified socket and return the file des from accepted socket*/
+   int file_des_new = accept(socket, addr, addrlen);
+   if(file_des_new == -1) {
+     error("Error while accepting a connection");
+   }
+   return file_des_new;
+ }
+
+ ssize_t readline(int file_des, void *str, size_t maxlen)
+ {
+   /* Read a line from the file descriptor with a maximum length in the specified buffer */
+     read(file_des, str, maxlen);
+ }
+
+ ssize_t sendline(int file_des, const void *str, size_t maxlen)
+ {
+   /* Write a line in the file descriptor with a maximum length with the given buffer */
+   int sent=0;
+   do {
+     sent += write(file_des, str + sent, maxlen - sent);
+   } while (sent != maxlen);
+ }
