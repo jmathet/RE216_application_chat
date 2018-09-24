0d8110386cae015aa06c1f11a4d0c386e4b0789b
Julien MATHET <jmathet@enseirb-matmeca.fr>
2018-09-24 18:43:32 +0200

    Correction erreurs if{}

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -22,8 +22,10 @@ struct sockaddr_in get_sockaddr(char** argv)
 int do_socket()
 {
   int sock = socket(AF_INET, SOCK_STREAM, 0); // TCP
-  if (sock == -1)
-    perror("socket"); exit(EXIT_FAILURE);
+  if (sock == -1){
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
   return sock;
 }
 
@@ -31,7 +33,15 @@ int do_connect(int sock, struct sockaddr_in host_addr)
 {
   int connect_result = connect(sock, (struct sockaddr *) &host_addr, sizeof(host_addr));
   if(connect_result == -1)
-    perror("connect"); exit(EXIT_FAILURE);
+  {
+    perror("connect");
+    exit(EXIT_FAILURE);
+  }
+}
+
+ssize_t readline(int file_des, void *str, size_t maxlen)
+{
+  return read(file_des, str, maxlen);
 }
 
 int main(int argc,char** argv)
