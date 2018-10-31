cb2089feec64ecc23714b0c183ae66cd3936b953
Théo LEPINE <theo.lepine@gmail.com>
2018-10-31 02:29:42 +0100

    Threads détachés + documentation + début nettoyage

--- sources/jalon04/src/include/server/server_tools.c
+++ sources/jalon04/src/include/server/server_tools.c
@@ -2,7 +2,6 @@
 
 void init_serv_addr(struct sockaddr_in *serv_addr, int port)
  {
-   /* Modify specified sockaddr_in for the server side with specified port */
    // clean structure
    memset(serv_addr, 0, sizeof(*serv_addr));
    serv_addr->sin_family = AF_INET; // IP V4
@@ -12,29 +11,23 @@ void init_serv_addr(struct sockaddr_in *serv_addr, int port)
 
  void do_bind(int socket, struct sockaddr_in addr_in)
  {
-   /* Perform a bind on specified socket */
    int bind_result = bind(socket, (struct sockaddr *) &addr_in, sizeof(addr_in));
-   if (bind_result == -1) {
-     error("Error during socket binding");
-   }
+   if (-1 == bind_result)
+     error("bind");
  }
 
  void do_listen(int socket, int nb_max)
  {
-   /* Switch specified socket in the listen state */
    int listen_result = listen(socket, nb_max);
-   if (listen_result == -1) {
-     error("Error during socket listening");
-   }
+   if (-1 == listen_result)
+     error("listen");
  }
 
  int do_accept(int socket, struct sockaddr *addr, socklen_t* addrlen)
  {
-   /* Accept a connection with the specified socket and return the file des from accepted socket*/
    int file_des_new = accept(socket, addr, addrlen);
-   if(file_des_new == -1) {
-     error("Error while accepting a connection");
-   }
+   if(-1 == file_des_new)
+     error("accept");
    return file_des_new;
  }
 
