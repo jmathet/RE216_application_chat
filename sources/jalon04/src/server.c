c7e2c8d8e41f6b4d6a06e4be44fe79da9417b821
Théo LEPINE <theo.lepine@gmail.com>
2018-10-31 09:18:27 +0100

    Réapplication commit "Threads détachés" corrigé

--- sources/jalon04/src/server.c
+++ sources/jalon04/src/server.c
@@ -6,11 +6,16 @@
 int main(int argc, char** argv)
 {
     /* INITS */
-    int port, enable=1;
-    int sock, connection_fd, status, nb_total_connections = 0;
+    int enable=1; // used for setsockopt
+    int port;
+    int sock;
+    int connection_fd;
+    int status;
+    int nb_total_connections = 0;
     struct sockaddr_in serv_addr;
     pthread_t thread;
     thread_arg * thread_input; // args for thread creation
+    // TODO a ameliorer
     struct users * first_user = malloc(sizeof(struct users));
     first_user->user_id = -1;
     first_user->pseudo = "";
@@ -26,23 +31,19 @@ int main(int argc, char** argv)
       exit(EXIT_FAILURE);
     }
 
-
     /* SOCKET SET-UP */
     sock = do_socket();
-    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1)
+    if(-1 == setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)))
       error("setsockopt");
     init_serv_addr(&serv_addr, port);
     do_bind(sock, serv_addr);
     do_listen(sock, USERS_NB_MAX);
     status=SERVER_RUNNING;
 
-    /* SIGNAL HANDLERS */
-    // TODO : gérer la fermeture du serveur correctement + nettoyage + gestion fin des threads
-
-    /* CHAT SERVER */
+    /* SERVER - main flow is accepting connections and creating threads */
     while(status != SERVER_QUITTING)
     {
-        // accept connection from client
+        // receiving new connections from clients
         socklen_t addrlen = sizeof(struct sockaddr);
         connection_fd = do_accept(sock, (struct sockaddr*)&serv_addr, &addrlen);
 
@@ -50,29 +51,39 @@ int main(int argc, char** argv)
         if(nb_total_connections >= USERS_NB_MAX) {// is the max numbers of clients reached ?
           printf("! Connection from a client closed : too many users.\n");
           send_int(connection_fd, SERVER_FULL);
-          close(connection_fd);
+          status = SERVER_QUITTING;
         }
-        else { // there is remaining slots for a new user
-          send_int(connection_fd, SERVER_RUNNING);
-          // thread args initialisation
+        else { // we accept the client
+          // malloc
           thread_input = (thread_arg*)malloc(sizeof *thread_input);
+
+          // sending OK signal with the pseudo-protocol
+          send_int(connection_fd, SERVER_RUNNING);
+
+          // thread args initialisation
           thread_input->thread_fd_connection = connection_fd;
           thread_input->linked_user_id = nb_total_connections + 1;
           thread_input->pt_nb_conn = &nb_total_connections;
           thread_input->users = first_user;
           thread_input->IP_addr = inet_ntoa(serv_addr.sin_addr);
           thread_input->port_number = ntohs(serv_addr.sin_port);
-          // thread creation
-          if( pthread_create( &thread, NULL ,  connection_handler , (void*)thread_input) != 0)
-          {
-              free(thread_input);
-              close(sock);
-              error("pthread_create");
-          }
-        } // END else
-      } // END while(1)
 
-    // CLEANING
+          // thread creation
+          if(0 != pthread_create( &thread, NULL ,  connection_handler , (void*)thread_input))
+              error("pthread_create");
+
+          // detach thread
+          if(0 != pthread_detach(thread))
+            error("pthread_detach");
+
+          // TODO free thread-specific inputs
+          // free(thread_input);
+
+        } // END else
+      } // END while(status != SERVER_QUITTING)
+
+    /* CLEAN UP */
     close(sock);
+
     return 0;
 }
